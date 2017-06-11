75f1d86f335061433f2c89939ba1faee58c87f7c
Awes Mubarak <awes.mubarak@awesmubarak.com>
2017-06-11 18:46:11 +0100

    Allow removal of packages

--- git-get.py
+++ git-get.py
@@ -6,8 +6,9 @@ import subprocess
 import yaml
 import sys
 
+
 def get_config():
-    with open(os.path.expanduser("~/.git_get.yml")) as file:
+    with open(os.path.expanduser("~/.git_get/config.yml")) as file:
         config = yaml.safe_load(file)
     return config
 
@@ -36,19 +37,42 @@ def set_logger():
 
 
 def install(package):
-    target = os.path.abspath(os.path.expanduser("~/pro/git-get/files"))
-    if not os.path.exists(target):
-        os.makedirs(target)
-    git("clone https://github.com/" + package)
+    """Installs packages"""
+    # Load package list
+    with open(os.path.expanduser("~/.git_get/packages.yml"), "r") as file:
+        package_list = yaml.safe_load(file)
+    # Install package or inform user already installed
+    if package not in package_list:
+        git("clone https://github.com/" + package)
+        # add to packages list
+        current_location = os.path.dirname(os.path.realpath(__file__))
+        package_list[package] = current_location + "/"+ package.split("/")[-1]
+        with open(os.path.expanduser("~/.git_get/packages.yml"), "w") as file:
+            file.write(yaml.dump(package_list, default_flow_style=False))
+    else:
+        print("Package already installed")
+
+
+def remove(package):
+    """Removes packages"""
+    with open(os.path.expanduser("~/.git_get/packages.yml"), "r") as file:
+        package_list = yaml.safe_load(file)
+    if package in package_list:
+        subprocess.run(["rm", package_list[package], "-r"], stdout=subprocess.PIPE)
+        del package_list[package]
+        with open(os.path.expanduser("~/.git_get/packages.yml"), "w") as file:
+            file.write(yaml.dump(package_list, default_flow_style=False))
+    else:
+        print("Package no installeds")
 
 
 def main(arguments):
     set_logger()
+    # get command
     if arguments[0] == "install":
-        config = get_config()
-        package = arguments[1]
-        logger.debug("Package to install: " + package)
-        install(package)
+        install(arguments[1])
+    elif arguments[0] == "remove":
+        remove(arguments[1])
     else:
         print("Invalid command")
 
